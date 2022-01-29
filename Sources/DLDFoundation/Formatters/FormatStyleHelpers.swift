357516aae795aa21ef6563917430f0a0ce8eafda
Dionne Lie-Sam-Foek <dionnelie@icloud.com>
2022-01-29 14:08:07 +0100

    - Added a few common FormatStyle helpers

---
+++ Sources/DLDFoundation/Formatters/FormatStyleHelpers.swift
@@ -0,0 +1,42 @@
+//
+//  FormatStyleHelpers.swift
+//  DLDFoundation
+//
+//  Created by Dionne Lie-Sam-Foek on 29/01/2022.
+//  Copyright © 2022 DiLieDevs. All rights reserved.
+//
+
+import Foundation
+
+public extension Date.ISO8601FormatStyle {
+    /// Returns only the year, month, and day in ISO8601 format style, i.e. `2022-01-29`.
+    func date() -> Self {
+        self.year().month().day()
+    }
+}
+
+public extension FloatingPointFormatStyle where Value: BinaryFloatingPoint {
+    /// Returns the binary floating point value with the given length of fractional digits.
+    /// - Parameter length: The number of digits after the decimal separator.
+    func fractions(_ length: Int) -> Self {
+        self.precision(.fractionLength(length))
+    }
+}
+
+public extension FormatStyle where Self == FloatingPointFormatStyle<Double>.Currency {
+    static func euros() -> Self {
+        currency(code: "EUR")
+    }
+    
+    static func usDollars() -> Self {
+        currency(code: "USD")
+    }
+    
+    static func localEuros() -> Self {
+        currency(code: Locale.current.currencyCode ?? "EUR")
+    }
+    
+    static func localUSDollars() -> Self {
+        currency(code: Locale.current.currencyCode ?? "USD")
+    }
+}
