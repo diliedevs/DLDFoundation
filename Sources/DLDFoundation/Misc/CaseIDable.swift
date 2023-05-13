b26c07ddc85a75ba6f5f81a36213c006fca3da90
Dionne Lie-Sam-Foek <dionnelie@icloud.com>
2023-05-13 12:40:44 +0200

    Added CaseIDable typealias for CaseIterable & Identifiable

---
+++ Sources/DLDFoundation/Misc/CaseIDable.swift
@@ -0,0 +1,11 @@
+//
+//  CaseIDable.swift
+//  
+//
+//  Created by Dionne Lie Sam Foek on 13/05/2023.
+//  Copyright © 2023 DiLieDevs. All rights reserved.
+//
+
+import Foundation
+
+public typealias CaseIDable = CaseIterable & Identifiable
